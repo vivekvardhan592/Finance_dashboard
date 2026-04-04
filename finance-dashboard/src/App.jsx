d0095df7f3a06501ec61bbae3ecb3a21164b3bd1
vivekvardhan592 <vivekvardhan592@gmail.com>
2026-04-04 12:50:48 +0530

    Initial commit

---
+++ finance-dashboard/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-blue-500">
+      <div className="bg-white text-black p-8 rounded-2xl shadow-xl hover:scale-105 transition">
+        Tailwind v3 Working 🚀
+      </div>
+    </div>
+  );
+}
+
+export default App;
